4fde566b3bf8bc69471fa6e8069d24d550739145
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-05-21 15:02:14 +0500

    Initial Commit

---
+++ Images App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Images App
+//
+//  Created by Nurgali on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
